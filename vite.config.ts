1ba298da6683865a794c2368300a08db73653bb0
junjianlin <junjianlin@xiaoman.cn>
2021-09-06 11:16:44 +0800

    chore: change vite setting

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from "path"
 import { chromeExtension } from "vite-plugin-chrome-extension"
 import styleImport from 'vite-plugin-style-import'
+import copy from 'rollup-plugin-copy'
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
@@ -12,15 +13,51 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      input: "src/manifest.json"
+      // input: "src/manifest.json"
+      input: {
+        popup: resolve(__dirname, 'src/popup/index.html'),
+        background: resolve(__dirname, 'src/background.ts'),
+        'content-scripts': resolve(__dirname, 'src/content-scripts/content-script.ts')
+      },
+      output: {
+        entryFileNames: '[name].js',
+      },
     }
   },
-  css: {
-    preprocessorOptions: {
-      less: {
-        javascriptEnabled: true,
+  // css: {
+  //   preprocessorOptions: {
+  //     less: {
+  //       javascriptEnabled: true,
+  //     },
+  //   },
+  // },
+  plugins: [vue(),  styleImport({
+    libs: [
+      {
+        libraryName: 'element-plus',
+        esModule: true,
+        ensureStyleFile: true,
+        resolveStyle: (name) => {
+          return `element-plus/lib/theme-chalk/${name}.css`
+        },
+        resolveComponent: (name) => {
+          return `element-plus/es/${name}`
+        },
       },
-    },
-  },
-  plugins: [vue(), chromeExtension()]
+    ],
+  }),
+    copy({
+      verbose: true,
+      hook: 'writeBundle',
+    targets: [
+      {
+        src: resolve(__dirname, 'src/manifest.json'),
+        dest: 'dist',
+      },
+      {
+        src: resolve(__dirname, 'src/assets'),
+        dest: 'dist'
+      }
+    ],
+  }),]
 })
