147326bd2a61f430c35aa3a80213a668d96ead36
junjianlin <junjianlin@xiaoman.cn>
2021-09-07 23:37:00 +0800

    feat: add rules setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { resolve } from "path"
-import { chromeExtension } from "vite-plugin-chrome-extension"
-import styleImport from 'vite-plugin-style-import'
-import copy from 'rollup-plugin-copy'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
+import { chromeExtension } from "vite-plugin-chrome-extension";
+import styleImport from "vite-plugin-style-import";
+import copy from "rollup-plugin-copy";
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
@@ -15,14 +15,17 @@ export default defineConfig({
     rollupOptions: {
       // input: "src/manifest.json"
       input: {
-        popup: resolve(__dirname, 'src/popup/index.html'),
-        background: resolve(__dirname, 'src/background.ts'),
-        'content-scripts': resolve(__dirname, 'src/content-scripts/content-script.ts')
+        popup: resolve(__dirname, "src/popup/index.html"),
+        background: resolve(__dirname, "src/background.ts"),
+        "content-scripts": resolve(
+          __dirname,
+          "src/content-scripts/content-script.ts"
+        ),
       },
       output: {
-        entryFileNames: '[name].js',
+        entryFileNames: "[name].js",
       },
-    }
+    },
   },
   // css: {
   //   preprocessorOptions: {
@@ -31,33 +34,40 @@ export default defineConfig({
   //     },
   //   },
   // },
-  plugins: [vue(),  styleImport({
-    libs: [
-      {
-        libraryName: 'element-plus',
-        esModule: true,
-        ensureStyleFile: true,
-        resolveStyle: (name) => {
-          return `element-plus/lib/theme-chalk/${name}.css`
+  plugins: [
+    vue(),
+    styleImport({
+      libs: [
+        {
+          libraryName: "element-plus",
+          esModule: true,
+          ensureStyleFile: true,
+          resolveStyle: (name) => {
+            return `element-plus/lib/theme-chalk/${name}.css`;
+          },
+          resolveComponent: (name) => {
+            return `element-plus/es/${name}`;
+          },
         },
-        resolveComponent: (name) => {
-          return `element-plus/es/${name}`
-        },
-      },
-    ],
-  }),
+      ],
+    }),
     copy({
       verbose: true,
-      hook: 'writeBundle',
-    targets: [
-      {
-        src: resolve(__dirname, 'src/manifest.json'),
-        dest: 'dist',
-      },
-      {
-        src: resolve(__dirname, 'src/assets'),
-        dest: 'dist'
-      }
-    ],
-  }),]
-})
+      hook: "writeBundle",
+      targets: [
+        {
+          src: resolve(__dirname, "src/manifest.json"),
+          dest: "dist",
+        },
+        {
+          src: resolve(__dirname, "src/rules.json"),
+          dest: "dist"
+        },
+        {
+          src: resolve(__dirname, "src/assets"),
+          dest: "dist",
+        },
+      ],
+    }),
+  ],
+});
