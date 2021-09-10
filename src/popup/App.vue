1c56d4a9f4bd1315e41172a5298064d8de083fd7
junjianlin <junjianlin@xiaoman.cn>
2021-09-10 09:41:21 +0800

    feat: add res-intercept page

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -1,9 +1,24 @@
 <script setup lang="ts">
+import { Component, ref } from 'vue'
 import UrlForm from './components/url-form.vue'
+import ResInterceptForm from './components/res-intercept-form.vue'
+
+const menuList = ref<{label: string, cmp: Component }[]>([
+  {
+    label: 'forward', cmp: UrlForm
+  },
+  {
+    label: 'response', cmp: ResInterceptForm
+  }
+])
 </script>
 
 <template>
-<UrlForm />
+<el-tabs tab-position="left">
+<el-tab-pane v-for="item in menuList" :key="item.label" :label="item.label">
+<component :is="item.cmp"></component>
+</el-tab-pane>
+</el-tabs>
 </template>
 
 <style>
---
+++ src/popup/components/res-intercept-form.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { StorageConnection } from '../utils/chrome-storage'
+import { openTab } from '../utils/chrome-api'
+const STORAGE_KEY = 'forward'
+const POPUP_PATH = 'src/popup/index.html'
+
+
+const openInNewTab = () => {
+    openTab(POPUP_PATH)
+}
+
+</script>
+
+<template>
+<div class="popup-head">
+    <h2>powerful tool</h2>
+     <el-tooltip content="新标签页中打开" placement="top" effect="light">
+        <el-button circle @click="openInNewTab" icon="el-icon-top-right"></el-button>
+    </el-tooltip>
+</div>
+ 
+</template>
+
+<style>
+.popup-head {
+    padding: 0 10px 10px 10px;
+        display: flex;
+    justify-content: space-between;
+}
+</style>
